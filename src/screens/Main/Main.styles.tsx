29b91fbb662dfb906b006ced22218436f06e991c
Shim MunSeong <Puterism.K@gmail.com>
2021-12-31 14:18:49 +0900

    feat: `Main` 스크린 UI 구현

---
+++ src/screens/Main/Main.styles.tsx
@@ -0,0 +1,115 @@
+import styled, { css } from '@emotion/native';
+import { GrayColor } from 'src/utils/color';
+
+interface FilterSelectProps {
+  active: boolean;
+}
+
+export const cafeListStyle = css`
+  margin: 0 16px;
+  padding-bottom: 24px;
+`;
+
+export const MainStyled = styled.View`
+  flex: 1;
+  background-color: ${GrayColor.GRAY_0};
+  padding-top: 56px;
+  z-index: 1;
+`;
+
+export const SearchInput = styled.TouchableOpacity`
+  height: 40px;
+  border: 1px solid ${GrayColor.GRAY_200};
+  border-radius: 20px;
+  padding: 0 12px;
+  margin: 0 16px;
+  margin-top: 8px;
+  margin-bottom: 24px;
+  justify-content: center;
+`;
+
+export const SearchInputPlaceHolder = styled.Text`
+  font-size: 14px;
+  color: ${GrayColor.GRAY_300};
+`;
+
+export const ScrolledListHeader = styled.View`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  padding: 16px 16px 24px 16px;
+  border-style: solid;
+  border-color: ${GrayColor.GRAY_100};
+  border-bottom-width: 1px;
+  background-color: ${GrayColor.GRAY_0};
+`;
+
+export const ScrolledListHeaderText = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const FilterChangeButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  padding: 16px 0;
+`;
+
+export const FilterChangeButtonText = styled.Text<FilterSelectProps>`
+  font-size: 12px;
+  font-weight: ${({ active }) => (active ? 'bold' : 'normal')};
+  color: ${GrayColor.GRAY_400};
+  margin-right: 4px;
+`;
+
+export const FilterSelect = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 8px 34px 34px 34px;
+`;
+
+export const FilterSelectItem = styled.TouchableOpacity<FilterSelectProps>`
+  margin-right: 8px;
+  padding: 12px 16px;
+  border-radius: 20px;
+  border: solid 1px ${({ active }) => (active ? '#ff9d33' : GrayColor.GRAY_100)};
+`;
+
+export const FilterSelectText = styled.Text<FilterSelectProps>`
+  font-size: 12px;
+  font-weight: ${({ active }) => (active ? 'bold' : 'normal')};
+`;
+
+export const Dimmed = styled.TouchableOpacity`
+  width: 100%;
+  height: 100%;
+  z-index: 11;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const DimmedArea = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(34, 34, 34, 0.1);
+`;
+
+export const IconWrapper = styled.View`
+  padding: 8px;
+  border-radius: 100px;
+  border: 1px solid #a7a7a7;
+  background-color: #cccccc;
+`;
+
+export const CafeListContainer = styled.View`
+  margin: 24px 16px;
+`;
+
+export const ListSeparator = styled.View`
+  margin-bottom: 8px;
+`;
--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,11 +1,164 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Animated, FlatList } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-const Main = () => {
+import DropDownArrowIcon from 'src/assets/icons/icon_dropdown_arrow.svg';
+import MapIcon from 'src/assets/icons/icon_map.svg';
+import SmallPersonIcon from 'src/assets/icons/icon_small_person.svg';
+import CafeListItem from 'src/components/CafeListItem/CafeListItem';
+import ErrorView from 'src/components/ErrorView/ErrorView';
+import FilterIllust from 'src/components/FilterIllust/FilterIllust';
+import Header from 'src/components/Header/Header';
+import FILTER from 'src/constants/filter';
+import { RootStackParamList } from 'src/navigators/types';
+import {
+  cafeListStyle,
+  Dimmed,
+  DimmedArea,
+  FilterChangeButton,
+  FilterChangeButtonText,
+  FilterSelect,
+  FilterSelectItem,
+  FilterSelectText,
+  IconWrapper,
+  ListSeparator,
+  MainStyled,
+  ScrolledListHeader,
+  ScrolledListHeaderText,
+  SearchInput,
+  SearchInputPlaceHolder,
+} from './Main.styles';
+import sampleCafeList from './sampleCafeList';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Main'>;
+
+const Main = ({ navigation: { navigate } }: Props) => {
+  const [currentFilter, setCurrentFilter] = useState(FILTER.NEAREST.id);
+  const [isFilterSelectOpen, setFilterSelectOpen] = useState(false);
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const geocode = '서울시';
+  const isError = false;
+  const permissionStatus = 'denied';
+
+  const toggleFilterSelect = () => {
+    setFilterSelectOpen((prevState) => !prevState);
+  };
+
+  const selectFilter = (filter: typeof currentFilter) => {
+    setCurrentFilter(filter);
+    setFilterSelectOpen(false);
+  };
+
   return (
-    <View>
-      <Text>Main</Text>
-    </View>
+    <>
+      <Header
+        left={
+          <Header.Button onPress={() => navigate('Main')}>
+            <IconWrapper>
+              <SmallPersonIcon />
+            </IconWrapper>
+          </Header.Button>
+        }
+        right={
+          <>
+            <FilterChangeButton onPress={toggleFilterSelect}>
+              <FilterChangeButtonText active={isFilterSelectOpen}>
+                {FILTER[currentFilter].name} 순
+              </FilterChangeButtonText>
+              <DropDownArrowIcon />
+            </FilterChangeButton>
+            <Header.Button onPress={() => navigate('Main')}>
+              <MapIcon />
+            </Header.Button>
+          </>
+        }
+        bottom={
+          isFilterSelectOpen && (
+            <FilterSelect>
+              <FilterSelectItem
+                active={currentFilter === FILTER.NEAREST.id}
+                onPress={() => selectFilter(FILTER.NEAREST.id)}
+              >
+                <FilterSelectText active={currentFilter === FILTER.NEAREST.id}>
+                  {FILTER.NEAREST.name} 순
+                </FilterSelectText>
+              </FilterSelectItem>
+              <FilterSelectItem
+                active={currentFilter === FILTER.MANY_COMMENTS.id}
+                onPress={() => selectFilter(FILTER.MANY_COMMENTS.id)}
+              >
+                <FilterSelectText active={currentFilter === FILTER.MANY_COMMENTS.id}>
+                  {FILTER.MANY_COMMENTS.name} 순
+                </FilterSelectText>
+              </FilterSelectItem>
+              <FilterSelectItem
+                active={currentFilter === FILTER.MANY_FAVORITES.id}
+                onPress={() => selectFilter(FILTER.MANY_FAVORITES.id)}
+              >
+                <FilterSelectText active={currentFilter === FILTER.MANY_FAVORITES.id}>
+                  {FILTER.MANY_FAVORITES.name} 순
+                </FilterSelectText>
+              </FilterSelectItem>
+            </FilterSelect>
+          )
+        }
+      />
+      {isFilterSelectOpen && (
+        <Dimmed activeOpacity={1} onPress={toggleFilterSelect}>
+          <DimmedArea />
+        </Dimmed>
+      )}
+      <MainStyled>
+        <View>
+          <SearchInput onPress={() => navigate('Main')}>
+            <SearchInputPlaceHolder>현위치 : {geocode}</SearchInputPlaceHolder>
+          </SearchInput>
+          <Animated.View style={{ opacity: fadeAnim }}>
+            <ScrolledListHeader>
+              <ScrolledListHeaderText>현위치에서 가장 {FILTER[currentFilter].name} 곳</ScrolledListHeaderText>
+            </ScrolledListHeader>
+          </Animated.View>
+        </View>
+        {isError ? (
+          <ErrorView>
+            <ErrorView.Heading>앗!</ErrorView.Heading>
+            <ErrorView.Message>카페 목록을 불러오지 못했어요!</ErrorView.Message>
+            {permissionStatus === 'denied' && (
+              <ErrorView.Message>카페 목록을 불러오기 위해서{'\n'}위치 권한을 허용해주세요</ErrorView.Message>
+            )}
+            <ErrorView.RetryButton>
+              <ErrorView.RetryText>다시 시도하기</ErrorView.RetryText>
+            </ErrorView.RetryButton>
+          </ErrorView>
+        ) : (
+          <FlatList
+            contentContainerStyle={cafeListStyle}
+            data={sampleCafeList}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => <ListSeparator />}
+            renderItem={({ item }) => {
+              const { id, name, road_addr } = item;
+              const distance = '1m';
+
+              const cafe = {
+                id: id,
+                title: name,
+                distance,
+                address: road_addr,
+                tags: [],
+                badges: [],
+              };
+
+              return <CafeListItem data={cafe} hasBorder />;
+            }}
+            ListHeaderComponent={<FilterIllust filter={currentFilter} />}
+            onEndReachedThreshold={0.5}
+          />
+        )}
+      </MainStyled>
+    </>
   );
 };
 
