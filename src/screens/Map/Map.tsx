015ef5e2223d1d0675073c8062982dc5bb3de133
Shim MunSeong <Puterism.K@gmail.com>
2022-01-31 22:44:06 +0900

    feat: `Map` 스크린에 네이버 지도 적용 및 임시 마커 추가

--- src/screens/Map/Map.tsx
+++ src/screens/Map/Map.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import NaverMapView, { Marker } from 'react-native-nmap';
 
 import ListIcon from 'src/assets/icons/icon_list.svg';
 import SmallPersonIcon from 'src/assets/icons/icon_small_person.svg';
+import mapPickerImage from 'src/assets/images/icon_mappicker.png';
+import mapPickerSelectImage from 'src/assets/images/icon_mappicker_select.png';
 import Header from 'src/components/Header/Header';
 import Typo from 'src/components/Typo/Typo';
 import useGeocode from 'src/hooks/useGeocode';
@@ -12,7 +14,7 @@ import { RootStackParamList } from 'src/navigators/types';
 import { GrayColor } from 'src/utils/color';
 import { FontType } from 'src/utils/font';
 
-import { MapStyled, mapViewStyle, SearchInput, IconWrapper, MapViewContainer, MapSafeArea } from './Map.styles';
+import { MapStyled, SearchInput, IconWrapper, MapViewContainer, MapSafeArea, mapViewStyle } from './Map.styles';
 
 const GANGNAM_STATION_COORDS = {
   latitude: 37.498095,
@@ -24,16 +26,10 @@ interface Props {
 }
 
 const Map = ({ navigation }: Props) => {
-  const mapRef = useRef<MapView>(null);
+  const mapRef = useRef<NaverMapView>(null);
 
   const { geolocation } = useGeolocation();
   const { geocode } = useGeocode(geolocation);
-
-  useEffect(() => {
-    if (!mapRef.current || !geolocation) return;
-
-    mapRef.current.setCamera({ center: geolocation });
-  }, [geolocation]);
 
   return (
     <MapStyled>
@@ -59,21 +55,15 @@ const Map = ({ navigation }: Props) => {
         </SearchInput>
       </MapSafeArea>
       <MapViewContainer>
-        <MapView
+        <NaverMapView
           ref={mapRef}
           style={mapViewStyle}
-          showsCompass={true}
-          showsUserLocation={true}
-          showsMyLocationButton={false}
-          moveOnMarkerPress={false}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: geolocation?.latitude ?? GANGNAM_STATION_COORDS.latitude,
-            longitude: geolocation?.longitude ?? GANGNAM_STATION_COORDS.longitude,
-            latitudeDelta: 0.009,
-            longitudeDelta: 0.009,
-          }}
-        />
+          showsMyLocationButton={true}
+          center={{ ...GANGNAM_STATION_COORDS, zoom: 16 }}
+        >
+          <Marker coordinate={GANGNAM_STATION_COORDS} image={mapPickerSelectImage} />
+          <Marker coordinate={GANGNAM_STATION_COORDS} image={mapPickerImage} />
+        </NaverMapView>
       </MapViewContainer>
     </MapStyled>
   );
